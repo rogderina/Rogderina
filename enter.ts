e6b10d90f7eb1a9fa81bec2e45bcbe7ee8988926
Rogderina <berlizovakaterina@mail.ru>
2021-12-08 13:02:37 +0700

    first

---
+++ enter.ts
@@ -0,0 +1,50 @@
+import { Builder, By, Key, until } from 'selenium-webdriver';
+import ('chromedriver');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+let selectors = require('./datas/selectors');
+require('./datas/data');
+import {urlD, mail, pass} from './datas/data';
+
+(async function name() {
+    //вход 
+    const driver = new Builder().forBrowser('chrome').build();
+
+     await driver.manage().window().maximize();
+     await driver.get(urlD);
+     await driver.sleep(5000);
+    
+      
+     await selectors.enter(driver).click();
+     await driver.sleep(1000);
+
+     await selectors.prinim(driver).click();
+     await driver.sleep(1000); 
+     await selectors.captcha(driver).click();
+     await driver.sleep(1000);
+    
+     await selectors.email(driver).sendKeys(mail);
+     await driver.sleep(1000);
+
+     await selectors.password(driver).sendKeys(pass);
+     await driver.sleep(1000);
+
+   
+     
+     
+
+     await selectors.enter2(driver).click();
+     await driver.sleep(1000);
+    
+    
+ 
+ 
+
+   
+ 
+   
+ 
+   
+ })();
