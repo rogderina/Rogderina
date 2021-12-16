a08ca9a267efd9f763a0448cc7e725b607611729
Rogderina <berlizovakaterina@mail.ru>
2021-12-16 16:36:34 +0700

    qwe

--- tests/pricelevel.ts
+++ tests/pricelevel.ts
@@ -4,17 +4,32 @@ const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
-
+let orderB = require('./sverkastak');
+let orderS = require('./sverkastakSell');
 
 class totalBalancAfterB {
-
+    balancesObject = await orderB.orderBuy(driver);
     balancBstr: number;
     balancBAfterBStr: number;
     totalB: number;
     print(){
-        console.log(`balancBuy: ${this.balancBstr}  balancAfterBuy: ${this.balancBAfterBStr} totalBuy: ${this.totalB}`);
+        console.log(balancBuy: ${this.balancBstr}  balancAfterBuy: ${this.balancBAfterBStr} totalBuy: ${this.totalB});
     }
     // toString(): string{
-    //     return `${this.name}: ${this.age}`;
+    //     return ${this.name}: ${this.age};
+
+}
+
+
+class totalBalancAfterS {
+    balancesObject = await orderS.orderSell(driver)
+    balancBstr: number;
+    balancBAfterBStr: number;
+    totalB: number;
+    print(){
+        console.log(balancSell: ${this.balancBstr}  balancAfterBuy: ${this.balancBAfterBStr} totalBuy: ${this.totalB});
+    }
+    // toString(): string{
+    //     return ${this.name}: ${this.age};
 
 }
